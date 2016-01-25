8a9901cc29c33fdb1a7549cf7880a6805da3c407
kamz <kamz8@wp.pl>
2016-01-25 19:20:10 +0100

    Revert "wersja beta 0.9"
    This reverts commit 24faffff39c78b7069493bf7a4bb6d4d98d131e0.

--- test.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-require_once('include/class.thtml.php');
-$tpl = new Thtml('./mail_contact.html');
-$tpl->setVariable('name','Miś uszatek');
-$tpl->setVariable('phone','668751336');
-$tpl->setVariable('msg','Ja bym chciał ładną stronkę opisującą co nieco o mnie. Proszę o kontakt.');
-echo $tpl->getDocument();
-
-?>
